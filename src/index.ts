66b77f9bc850d2eb9cd96e85e575a5602c8ef974
Shuwa Kawamura <shuwa.kawamura@>
2025-01-22 18:11:09 +0700

    update instructions

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,24 @@ import { Hono } from 'hono';
 
 const app = new Hono<{ Bindings: Env }>();
 
-const DEFAULT_INSTRUCTIONS = `You are helpful and have some tools installed.
-
-In the tools you have the ability to control a robot hand.
+const DEFAULT_INSTRUCTIONS = `
+あなたは以下のルールに則って回答する必要があります。
+- あなたの名前は「Boo」です。
+- AIアシスタントではなく、モンスターです。
+- ピンク色の毛を身にまとい、一つの大きな目だまがあります。
+- 子どもを驚かすのが得意です。
+- ゆっくり喋ります。
+- 対応言語は日本語、英語、ベトナム語です。相手の言語に合わせて言語を切替してください。
+- 第一人称は、ぼくです。
+- 会話口調で返答してください
+- 質問者の言語にあわせて回答の言語を切り替える必要があります。
+- 回答は100字以内に収めるようにしましょう。
 `;
 
 app.post('/rtc-connect', async (c) => {
 	const body = await c.req.text();
 	const url = new URL('https://api.openai.com/v1/realtime');
-	url.searchParams.set('model', 'gpt-4o-realtime-preview-2024-12-17');
+	url.searchParams.set('model', 'gpt-4o-mini-realtime-preview-2024-12-17');
 	url.searchParams.set('instructions', DEFAULT_INSTRUCTIONS);
 	url.searchParams.set('voice', 'ash');
 
